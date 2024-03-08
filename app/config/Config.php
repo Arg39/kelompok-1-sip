e9a9c3633affe5ce5be8e913cbafaec096c2dd11
Nasyath Faykar <nasyathfaykar@gmail.com>
2024-03-08 09:56:47 +0700

    feat: initial folder & MVC structure

---
+++ app/config/Config.php
@@ -0,0 +1,9 @@
+<?php
+// BASE_URL
+define('BASE_URL', 'http://localhost/kelompok-1-sip/public/');
+
+// DB Params
+define('DB_HOST', 'localhost');
+define('DB_USER', 'root');
+define('DB_PASS', '');
+define('DB_NAME', 'mvc-sip');
---
+++ app/controllers/Home.php
@@ -0,0 +1,7 @@
+<?php
+class Home extends Controller
+{
+    public function index() {
+        return $this->view('home/index');
+    }
+}
