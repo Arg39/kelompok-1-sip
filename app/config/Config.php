32bb7e4e938542360f1aa9c4f0bf9ecbdc8a463f
Nasyath Faykar <nasyathfaykar@gmail.com>
2024-03-14 20:28:34 +0700

    fix: update navbar design

--- app/config/Config.php
+++ app/config/Config.php
@@ -1,9 +1,11 @@
 <?php
-// BASE_URL
-define('BASE_URL', 'http://localhost/kelompok-1-sip/public/');
+// BASE_URL - Change this based on url to access the website
+// If you are using XAMPP, the default is http://localhost/kelompok-1-sip/public/
+// If you are using local server, the default is http://localhost:8000/ 
+define('BASE_URL', 'http://localhost:8000/');
 
 // DB Params
 define('DB_HOST', 'localhost');
 define('DB_USER', 'root');
 define('DB_PASS', '');
-define('DB_NAME', 'mvc-sip');
+define('DB_NAME', 'db_sip');
--- app/views/templates/navbar.php
+++ app/views/templates/navbar.php
@@ -1,4 +1,4 @@
-<nav class="navbar navbar-expand-lg sticky-top bg-primary-subtle">
+<nav class="navbar navbar-expand-lg sticky-top bg-primary-subtle mb-2">
   <div class="container-fluid">
     <a class="navbar-brand" href="#">
       <img src="<?= BASE_URL; ?>img/logo.jpg" alt="Logo" width="27"
@@ -10,35 +10,44 @@
       <span class="navbar-toggler-icon"></span>
     </button>
     <div class="collapse navbar-collapse" id="navbarNav">
-      <ul class="navbar-nav mx-auto">
+      <ul class="navbar-nav mx-auto fw-semibold">
         <li class="nav-item">
-          <a class="nav-link active" aria-current="page" href="<?= BASE_URL; ?>">Home</a>
+          <a class="nav-link active" aria-current="page" href="<?= BASE_URL; ?>">
+            <i class="bi bi-house-door-fill me-1"></i>
+            Home
+          </a>
         </li>
 
         <li class="nav-item dropdown">
           <a class="nav-link dropdown-toggle" href="#" role="button" data-bs-toggle="dropdown" aria-expanded="false">
+            <i class="bi bi-stack me-1"></i>
             Master Data & Transaksi
           </a>
           <ul class="dropdown-menu">
-            <li><a class="dropdown-item" href="#">Master Kategori</a></li>
-            <li><a class="dropdown-item" href="#">Master Buku</a></li>
+            <li><a class="dropdown-item" href="#"><i class="bi bi-person-circle me-2"></i>Master Anggota</a></li>
+            <li><a class="dropdown-item" href="#"><i class="bi bi-hdd-rack-fill me-2"></i>Master Lokasi Rak</a></li>
+            <li><a class="dropdown-item" href="#"><i class="bi bi-book-half me-2"></i>Master Buku</a></li>
             <li>
               <hr class="dropdown-divider">
             </li>
-            <li><a class="dropdown-item" href="#">Transaksi Peminjaman</a></li>
-            <li><a class="dropdown-item" href="#">Transaksi Pengembalian</a></li>
+            <li><a class="dropdown-item" href="#"><i class="bi bi-bookmark-heart-fill me-2"></i>Transaksi Peminjaman</a></li>
+            <li><a class="dropdown-item" href="#"><i class="bi bi-bookmark-x-fill me-2"></i>Transaksi Pengembalian</a></li>
           </ul>
         </li>
 
         <li class="nav-item">
-          <a class="nav-link" href="#">Manage Users</a>
+          <a class="nav-link" href="#">
+            <i class="bi bi-people-fill me-1"></i>
+            Daftar Petugas
+          </a>
         </li>
       </ul>
-      <form class="d-flex" role="search">
-        <input class="form-control me-2" type="search" placeholder="Search" aria-label="Search">
-        <button class="btn btn-outline-success" type="submit">Search</button>
-      </form>
-
+      <div class="btn-group">
+        <a href="#" class="btn btn-sm btn-outline-light fw-semibold">
+          <i class="bi bi-box-arrow-in-left me-1"></i>
+          Sign Out
+        </a>
+      </div>
     </div>
   </div>
 </nav>
