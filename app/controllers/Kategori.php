350b6687eb0e3094d0d9e631bf8ca0d35be1b58c
Az Zikra Ismunandar <129355051+azzikra1s@users.noreply.github.com>
2024-03-17 03:01:38 +0700

    Add CRUD functionalities in `Kategori.php`
    Implemented CRUD operations (tambah, hapus, ubah, cari) in the `Kategori` controller for managing categories.

--- app/controllers/Kategori.php
+++ app/controllers/Kategori.php
@@ -14,8 +14,53 @@ class Kategori extends Controller
 
     public function tambah() {
         if ($this->model('KategoriModel')->buatKategori($_POST) > 0) {
+            $this->flash->setFlashMessage('berhasil', 'ditambahkan', 'success');
+            header('Location:' . BASE_URL . 'kategori');
+            exit;
+        } else {
+            $this->flash->setFlashMessage('gagal', 'ditambahkan', 'danger');
             header('Location:' . BASE_URL . 'kategori');
             exit;
         }
     }
+
+    public function hapus($id)
+    {
+        if( $this->model('kategoriModel')->hapusKategori($id) > 0 ) {
+            $this->flash->setFlashMessage('berhasil', 'dihapus', 'success');
+            header('Location: ' . BASE_URL . 'kategori');
+            exit;
+        } else {
+            $this->flash->setFlashMessage('gagal', 'dihapus', 'danger');
+            header('Location: ' . BASE_URL . 'kategori');
+            exit;
+        }
+    }
+
+    public function getubah() {
+        echo json_encode($this->model('kategoriModel')->getCategoryId($_POST['id']));
+    }
+
+    public function ubah() {
+        if( $this->model('kategoriModel')->ubahKategori($_POST) > 0 ) {
+            $this->flash->setFlashMessage('berhasil', 'diubah', 'success');
+            header('Location: ' . BASE_URL . 'kategori');
+            exit;
+        } else {
+            $this->flash->setFlashMessage('gagal', 'diubah', 'danger');
+            header('Location: ' . BASE_URL . 'kategori');
+            exit;
+        } 
+    }
+
+    public function cari() {
+        $data = [
+            'title' => 'Kategori',
+            'kategori' => $this->model('KategoriModel')->cariKategori()
+        ];
+        return $this->view('templates/header', $data)
+             . $this->view('kategori/index', $data)
+             . $this->view('templates/footer', $data);
+    }
+
 }
