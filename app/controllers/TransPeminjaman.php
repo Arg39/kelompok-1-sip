c2d938d32551f0e951aa0153b7f52ec162f42163
faykarr <nasyathfaykar@gmail.com>
2024-03-17 23:16:02 +0700

    feat: finished re-design detail view in detail peminjaman

--- app/controllers/TransPeminjaman.php
+++ app/controllers/TransPeminjaman.php
@@ -10,8 +10,11 @@ class TransPeminjaman extends Controller {
 
     public function fromdetail($id)
     {
-        $data['title'] = 'detail Data  Peminjaman';
-        $data['detaildata'] = $this->model('TransPeminjamanModel')->getDetailPeminjaman($id);
+        $data = [
+            'title' => 'Detail Peminjaman | Perpusku',
+            'detaildata' => $this->model('TransPeminjamanModel')->getDetailPeminjaman($id),
+            'bukuDiPinjam' => $this->model('TransPeminjamanModel')->getBukuDipinjam($id),
+        ];
         $this->view('templates/header', $data);
         $this->view('peminjaman/detail', $data);
         $this->view('templates/footer');
